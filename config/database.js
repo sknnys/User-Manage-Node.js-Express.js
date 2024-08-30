d7944c18d964ef40d5c4db0b3e9e9f1cf3bb8ab8
HudrsonSilvaHS <155266342+sknnys@users.noreply.github.com>
2024-08-30 18:31:38 +0000

    test1

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'sqlite', // Or your chosen database
+    dialectOptions: { 
+        ssl: {
+            require: true, 
+            rejectUnauthorized: false // Only for development, use a secure setup in production
+        }
+    } 
 });
 
 module.exports = sequelize;
